f5f6dd6d620bdf482013f6d522696b64907bcb73
tamar <tamar@TAMARA.local>
2023-08-23 10:26:43 +0200

    feature: add location and image model

---
+++ app/Models/Image.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models;
+
+class Image {
+    public $url;
+    public $webp_url;
+    public $alt;
+}
---
+++ app/Models/Location.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Image;
+
+class Location {
+    public $hashid;
+    public $name;
+    public $getFeaturedImageModelImageInstance;    
+}
