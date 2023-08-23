adcf87b8563eb818c53d6ca8381cfe570d55c4f6
tamar <tamar@TAMARA.local>
2023-08-23 10:48:57 +0200

    remove cache views

--- storage/framework/views/74fe326cd8756bb3d4ad26b713a18b86.php
+++
@@ -1,6 +0,0 @@
-<?php if (! empty(trim($__env->yieldContent('title')))): ?>
-    <title><?php echo $__env->yieldContent('title'); ?> - <?php echo e(config('app.name')); ?></title>
-<?php else: ?>
-    <title><?php echo e(config('app.name')); ?></title>
-<?php endif; ?>
-<?php /**PATH /home/vagrant/new-sites/reykjavik-auto/resources/views/layouts/partials/default-web-title.blade.php ENDPATH**/ ?>
