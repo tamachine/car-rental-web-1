ada4c7d6b5098d8f1eb6c14eb2a0b4106e722783
tamar <tamar@TAMARA.local>
2023-08-28 16:01:33 +0200

    feature: create Feature component

---
+++ app/View/Components/Feature.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Feature extends Component
+{
+    /**
+     * The text
+     *
+     * @var string
+     */
+    public $text;
+
+    /**
+     * The title
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * The image path
+     *
+     * @var string
+     */
+    public $imagePath;
+
+    /**
+     * The webp image path
+     *
+     * @var string
+     */
+    public $webpImagePath;
+
+    /**
+     * The flex direction on desktop.
+     *
+     * @var string
+     */
+    public $reversed;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($title, $text, $imagePath, $webpImagePath = null, $reversed = false)
+    {
+        $this->title = $title;
+        $this->text = $text;
+        $this->imagePath = $imagePath;
+        $this->webpImagePath = $webpImagePath;
+        $this->reversed = $reversed;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.feature');
+    }
+}
---
+++ resources/views/components/feature.blade.php
@@ -0,0 +1,32 @@
+@if ($reversed) 
+    <div class="flex flex-col md:flex-row-reverse md:text-left gap-5 md:gap-12 lg:gap-20 items-start lg:items-stretch">    
+        <div class="image-wrapper md:flex-1 md:rounded-2xl w-full">                  
+            <x-webp-image :image-path="$imagePath" :webp-image-path="$webpImagePath" />
+        </div>
+        <h2 class="md:hidden block md:text-left">{!! $title !!}</h2>
+        <div class="md:flex-1 flex flex-col md:items-start gap-7">
+            <h2 class="hidden md:block md:text-left">{!! $title !!}</h2>
+            <div>
+                <x-read-more-block text="{!! $text !!}" />    
+            </div>
+            <button class="tab hidden md:block">{!! __('home.feature-more') !!}</button>
+        </div>
+    </div>
+@else
+    <div class="flex flex-col md:flex-row md:text-right gap-5 md:gap-12 lg:gap-20 items-start lg:items-stretch">    
+        <div class="image-wrapper md:flex-1 md:rounded-2xl w-full">              
+            <x-webp-image :image-path="$imagePath" :webp-image-path="$webpImagePath" />  
+        </div>
+
+        <div class="md:flex-1 flex flex-col md:items-end gap-7">
+            <h2 class="md:text-right md:pr-0 pr-28">{!! $title !!}</h2>
+            <div class="">
+                <x-read-more-block text="{!! $text !!}" :reverse-text-align=true />    
+            </div>
+            <button class="tab hidden md:block">{!! __('home.feature-more') !!}</button>
+        </div>
+    </div>
+@endif
+
+
+
