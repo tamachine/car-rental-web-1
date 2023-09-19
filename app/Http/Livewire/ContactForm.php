ae6dbc4ade6e71fe1a407b986050b5cc93b3565f
tamar <tamar@TAMARA.local>
2023-09-19 14:09:05 +0200

    fix: in order to call modal livewire component, contact form must have a contact submit livewire component

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -7,80 +7,14 @@ use App\Interfaces\ContactFormRepositoryInterface;
 
 class ContactForm extends Component
 {
-    /*
-    ***************************************************************
-    ** PROPERTIES
-    ***************************************************************
-    */
-
-    /**
-     * @var string
-     */
-    public $name;
-
-    /**
-     * @var string
-     */
-    public $email;
-
-    /**
-     * @var string
-     */
-    public $subject;
-
-    /**
-     * @var string
-     */
-    public $type;
-
-    /**
-     * @var string
-     */
-    public $message;    
-
-     /**
-     * @var bool
-     */
-    public $submitButtonCentered;
-
-    public $modalId;
-
-    /*
-    ***************************************************************
-    ** METHODS
-    ***************************************************************
-    */
-
-    public function mount(bool $submitButtonCentered = true)
+    public function mount()
     {
-        $this->type = 'general';
-        $this->submitButtonCentered = $submitButtonCentered;
+        //
     }
-
+    
     public function render()
     {
         return view('livewire.contact-form');
     }
 
-    public function send(ContactFormRepositoryInterface $contactFormRepository)
-    {
-        $this->dispatchBrowserEvent('validationError');
-
-        $params = $this->validate([
-            'name'      => ['required'],
-            'email'     => ['required', 'email'],
-            'subject'   => ['required'],
-            'type'      => ['required'],
-            'message'   => ['required'],
-        ]);
-
-        $contactFormRepository->send($params);
-
-        $this->reset(['name', 'email', 'subject','message']);
-      
-        $this->type = "general";
-
-        $this->emit('openModal:'.$this->modalId); 
-    }
-  
 }
