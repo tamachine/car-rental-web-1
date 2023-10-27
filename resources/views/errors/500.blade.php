d605fa0de2fc52b5bf7c0d46b8625e99e61e59b3
tamar <tamar@TAMARA.local>
2023-10-27 07:56:54 +0200

    there is no image component. Remove it and use webp image component. 403 error blade updated.

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     <div class="h-[calc(100vh_-_62px)] md:h-[calc(100vh_-_76px)] w-fill-screen text-white">
         <div class="bg-image image-wrapper">
-            <x-image path="images/404.jpg"/>
+            <x-webp-image :image-path="asset('images/404.jpg')" :webp-image-path="asset('images/404.webp')"/>
         </div>
         <div class="relative flex md:flex-row flex-col justify-between md:justify-center mx-auto w-full md:h-auto h-full gap-1 md:gap-6 items-center md:text-left text-center md:p-0 pb-10">
             <div class="text-[128px] md:text-[220px] font-fredoka-semibold flex flex-col">
