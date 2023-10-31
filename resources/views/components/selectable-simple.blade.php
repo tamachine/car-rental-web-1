9a77ad303f5832ea7d4b1b24c8bb4ae2e9351c2d
tamar <tamar@TAMARA.local>
2023-10-31 18:37:25 +0900

    create selectable-simple component and add landing linkgs

--- resources/views/components/selectable-simple.blade.php
+++ resources/views/components/selectable-simple.blade.php
@@ -13,47 +13,44 @@
 >
     <input type="text" x-model="value" class="hidden"/>
     <div 
-        class="relative group"       
+        class="relative group"
+        x-on:mouseover.away="close()"
         >
         <div
             class="
                 flex justify-between  
-                gap-1 md:gap-5 p-3 border border-gray-secondary group-hover:bg-pink-red-secondary"
+                gap-1"
             :class="{ 
                 'rounded-t-lg': show, 
                 'rounded-lg': !show,
-                'bg-pink-red-secondary': isSelected }"
-            
-            x-on:click="toggleVisibility()"
-                    
+                'bg-pink-red-secondary': isSelected }"                        
+            x-on:mouseover="open()"
             >
             <div class="flex gap-2 justify-center items-center">
                 <img src="{{ $iconPath }}" />
                 <button x-text="selectedTitle">{!! $title !!}</button>
             </div>
             <div>
-                <img src="{{ asset('images/icons/arrow-down.svg') }}" class="inline w-3" x-show="!show"  />                
-                <img src="{{ asset('images/icons/arrow-up.svg') }}" class="inline w-3" x-cloak x-show="show" />               
+                <img src="{{ asset('images/icons/arrow-down.svg') }}" class="inline w-3"  />               
             </div>
         </div>
         <div
-            class="absolute rounded-b-lg w-full border-gray-secondary border border-t-0 bg-white z-10"
+            class="absolute rounded-b-lg inline-block whitespace-nowrap left-1/2 transform -translate-x-1/2 bg-white z-10"
             x-cloak
             x-show="show"
         >
             <ul class="divide-y">
                 @foreach($items as $selectableFullItem)
                     <li
-                        x-on:click="clickItemOnFullMode({{ $selectableFullItem->toJson() }})"
-                        wire:click="{{ isset($itemWireClickEvent) ? $itemWireClickEvent.'('.$selectableFullComponent->toJson().','.$selectableFullItem->toJson().')' : '' }}"
+                        x-on:click="clickItemOnSimpleMode({{ $selectableFullItem->toJson() }})"                        
                         class="
                             cursor-pointer
-                            py-2 pl-9
+                            py-2 px-3
                             transition ease-in-out duration-300
                             {{ $loop->last ? 'hover:rounded-b-lg ' : '' }}   
-                           hover:bg-pink-red hover:text-white                                           
+                            hover:text-pink-red                                          
                             "
-                        :class="{ 'bg-pink-red text-white' : (selectedValue == '{{ $selectableFullItem->value }}') }"
+                        :class="{ 'text-pink-red' : (selectedValue == '{{ $selectableFullItem->value }}') }"
                     >
                     {!! $selectableFullItem->text !!}
                     </li>
@@ -62,6 +59,4 @@
         </div>
     </div>
 </div>
-
-<input wire:model="selectables.{{ $wireModel }}"  type="hidden">
 
