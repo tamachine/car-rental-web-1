58fd15ffa4814e30408cc3edda2409da660cbb2f
tamar <tamar@TAMARA.local>
2023-11-14 16:19:05 +0900

    show minus plus input or select button depending on extras max units

--- resources/views/components/extra/col-right.blade.php
+++ resources/views/components/extra/col-right.blade.php
@@ -4,10 +4,13 @@
             @include('components.extra.price')
         </div>
         <div>
-            @include('components.extra.button')
 
-            <x-plus-minus-input field="extra" bg="bg-black" text="text-white" :show-number="false" class="w-[109px] gap-0 px-2 py-4 rounded-md h-8 mx-auto" />
-
+            @if($extra['max_units'] > 1 )
+                <x-plus-minus-input field="extra" bg="bg-black" text="text-white" :show-number="false" class="w-[109px] gap-0 px-2 py-4 rounded-md h-8 mx-auto" />
+            @else
+                @include('components.extra.button')
+            @endif
+            
         </div>
     </div>
 </div>
