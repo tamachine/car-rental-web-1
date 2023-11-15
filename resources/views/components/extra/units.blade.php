f0caf49ca9dfcfd9afbd6b1d1d8505d9ef0b4f26
tamar <tamar@TAMARA.local>
2023-11-15 21:07:49 +0900

    show extras units in mobile version

--- resources/views/components/extra/units.blade.php
+++ resources/views/components/extra/units.blade.php
@@ -4,4 +4,6 @@
 
 <div id="extra-units-{{ $id }}" class="font-fredoka-semibold text-[40px] md:h-12">{{ $units }}</div>
 
-<div class="text-sm">{!! $name !!}</div>
+<div class="md-max:hidden">
+    <div class="text-sm">{!! $name !!}</div>
+</div>
